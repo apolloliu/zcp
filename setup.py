fbdb7abfb53957af7cc28b1e32f1749833e7fc81
Hanxi_Liu <hanxi.liu@easystack.cn>
2017-04-17 20:09:45 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,10 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
-    name="zcp",
-    version="1.0.0",
+    version="1.0.1",
     author="hanxi.liu",
     author_email="apolloliuhx@gmail.com",
     packages=find_packages(),
     scripts=['bin/eszcp-polling'],
-    url="https://github.com/apolloliu/ZCP",
     description="A Timer task for polling ceilometer metrics into zabbix"
 )
